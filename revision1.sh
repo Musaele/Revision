ebae1b04644a5cca0aab166409816e1f0a376f0e
Musaele <104670940+Musaele@users.noreply.github.com>
2024-07-04 19:09:44 +0500

    Update revision1.sh

--- revision1.sh
+++ revision1.sh
@@ -9,7 +9,7 @@ echo "ProxyName: $ProxyName"
 echo "ENV: $ENV"
 
 # Set the path to your service account JSON key file
-KEY_FILE=".secure_files/abacus-apigee-demo-a9fffc7cc15c.json"
+KEY_FILE=".secure_files/service-account.json"
 
 echo "$KEY_FILE"
 
