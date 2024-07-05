f4378326dcd4c33c33043a13bbdae6a734ac7c91
Musaele <104670940+Musaele@users.noreply.github.com>
2024-07-05 14:43:41 +0500

    Update revision1.sh

--- revision1.sh
+++ revision1.sh
@@ -33,7 +33,8 @@ fi
 echo "Access Token: $access_token"
 
 # Save the access token in the environment file
-echo "access_token=$access_token" > .secure_files/build.env
+#echo "access_token=$access_token" > .secure_files/build.env
+echo "access_token=$access_token" >> $GITHUB_ENV
 
 # Get stable_revision_number using access_token
 revision_info=$(curl -H "Authorization: Bearer $access_token" "https://apigee.googleapis.com/v1/organizations/$ORG/environments/$ENV/apis/$ProxyName/deployments")
